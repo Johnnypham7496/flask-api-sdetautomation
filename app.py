454971b01b64e39aed9e8b9b0e1a5980a5626c23
Johnnpham7496 <johnpham7496@gmail.com>
2023-09-27 12:56:04 -0500

    added create_db function in app.py

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Response, Flask, jsonify
 from config import my_app
+from models.user_model import *
+
 
 
 app = Flask(__name__)
+
+
+def create_db():
+    db.create_all()
+    User.addd_user_td()
+    response_text = '{ "message": "Database created." }'
+    response = Response(response_text, 200, mimetype='application/json')
+    return response
 
 
 @app.get('/')
