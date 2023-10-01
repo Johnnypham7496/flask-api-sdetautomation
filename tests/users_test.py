a5f5271d0aa257155d3a78dce00f9c3d51616aa0
Johnnpham7496 <johnpham7496@gmail.com>
2023-10-01 00:27:21 -0500

    fixed app connection for users_test.py

--- tests/users_test.py
+++ tests/users_test.py
@@ -7,11 +7,11 @@ from config import my_app
 from db_config import db 
 
 flask_app = connexion.FlaskApp(__name__)
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(my_app.root_path, 'database/database.db')
-my_app.app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-my_app.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(my_app.root_path, 'database/test.db')
+flask_app.app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+flask_app.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(flask_app.app)  
-my_app.add_api('swagger.yml')
+flask_app.add_api('../swagger.yml')
 
 
 @pytest.fixture(scope='module')
@@ -25,3 +25,14 @@ def helper(json_info):
     for info in json_info:
         first_row = info.decode("utf-8")
         return str(json.loads(first_row))
+
+
+def test_0001_get(client):
+    td_username = 'thor'
+
+    response = client.get(f'/users/v1/{td_username}')
+
+    assert response.status_code == 200
+
+    # json_info = helper(response.response)
+
